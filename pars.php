ff114b872a5cea77f10ef9625c4965f7b7211b68
Marina Oladko <36618325+Ivi-Mary@users.noreply.github.com>
2018-06-03 19:34:02 +0300

    Update pars.php

--- pars.php
+++ pars.php
@@ -1,11 +1,42 @@
 <?php
-header('Content-Type: text/html; charset=UTF-8');
- require('phpQuery-master/phpQuery/phpQuery.php');
 
- $url = 'https://privatbank.ua/';
- $file = file_get_contents($url);
- 
- $pattern='#<table id = "course-table-pb.+?</table>#s';
- preg_match( $pattern,$file,$matches);
- 
- print_r($matches);
+$styles = array(
+	'big_room_house',
+	'club_house',
+	'dance_pop',
+	'deep_house',
+	'electrohouse',
+	'future_house',
+	'g_house',
+	'pop',
+	'progressive_housee',
+	'russian_pop',
+	'techhouse',
+);
+$styles = array(
+	'techhouse',
+);
+
+$urls = array();
+
+foreach ($styles as $style) {
+	for ($m=1; $m < 12; $m++) { 
+		$urls[] = "http://promodj.com/music/$style/?sortby=rating&bitrate=high&no_junk=1&period=date&duration=10m&year=2017&month=$m&page=1";
+		$urls[] = "http://promodj.com/music/$style/?sortby=rating&bitrate=high&no_junk=1&period=date&duration=10m&year=2017&month=$m&page=2";
+	}
+}
+
+foreach ($urls as $url) {
+	$html = file_get_contents($url);
+
+	$re = '/<span class="downloads_count"><a onclick="return cb\(event\);" href="(.*)" ambatitle="Download">/im';
+
+	preg_match_all($re, $html, $matches, PREG_SET_ORDER, 0);
+
+	foreach ($matches as $key => $value) {
+		echo $value[1]."\r\n";
+	}
+
+}
+
+?>
